6c9b3e91e7ac741b74d1b139fdede4d960db0f60
Sangeeth Kumar <sangeeth.palanichamy@gmail.com>
2023-03-09 17:54:12 +0530

    forgot to push

--- backend/survey/feedback/admin.py
+++ backend/survey/feedback/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+
 admin.site.register(QuestionType)
 admin.site.register(Question)
-admin.site.register(SurveyQuestionThrough)
-admin.site.register(SurveyDetail)
 
+
+class SurveyQuestionThroughInline(admin.StackedInline):
+    model = SurveyQuestionThrough
+    extra = 0
+
+
+class SurveyDetailAdmin(admin.ModelAdmin):
+    inlines = [SurveyQuestionThroughInline]
+
+
+admin.site.register(SurveyDetail, SurveyDetailAdmin)
